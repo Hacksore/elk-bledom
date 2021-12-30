0dd8b4cd6d5ef0586ad547b777558a184f6760da
Sean Boult <996134+Hacksore@users.noreply.github.com>
2021-12-30 10:06:59 -0600

    Add icons

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,42 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({
+    includeManifestIcons: true,
+    manifest: {
+      name: "Office Lights",
+      short_name: "Office Lights",
+      icons: [
+        {
+          "src": "icon-256.png",
+          "sizes": "256x256",
+          "type": "image/png"
+        },
+        {
+          "src": "icon-120.png",
+          "sizes": "120x120",
+          "type": "image/png"
+        },
+        {
+          "src": "icon-180.png",
+          "sizes": "180x180",
+          "type": "image/png"
+        },
+        {
+          "src": "icon-192.png",
+          "sizes": "192x192",
+          "type": "image/png"
+        },
+        {
+          "src": "icon-64.png",
+          "sizes": "64x64",
+          "type": "image/png"
+        }
+      ],
+    }
+  })],
   server: {
     proxy: {
       "/api": "http://192.168.2.102:80/",
